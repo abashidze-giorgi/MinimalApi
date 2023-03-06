1c86868ff109a79881eaed9e87fe4e100776c274
giorgi abashidze <gabashidze99@gmail.com>
2023-03-06 15:59:11 +0400

    Clean Code

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,5 +1,5 @@
-using Microsoft.EntityFrameworkCore;
 using MinimalApi;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
--- MinimalApi/SuperHero.cs
+++ MinimalApi/SuperHero.cs
@@ -3,9 +3,8 @@
     public class SuperHero
     {
         public int Id { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string HeroName { get; set; } = string.Empty;
-
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? HeroName { get; set; }
     }
 }
